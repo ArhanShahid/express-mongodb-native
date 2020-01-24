0b9003b5d2d934025c23bb8758ed1a02515a7f97
unknown <arhanshahid@gmail.com>
2020-01-24 16:12:19 +0500

    event api

--- controller/dataCtrl.js
+++ controller/dataCtrl.js
@@ -18,3 +18,42 @@ exports.reportsData = async (req, res) => {
         res.status(200).json(helper.error_message(e));
     }
 }
+
+exports.eventData = async (req, res) => {
+    try {
+        if (!req.body.month && !req.body.num) {
+            res.status(200).json(helper.error_message('Required Data Missing.'));
+        } else {
+            res.status(200).json(helper
+                .success_message(eventGenerator(Number(req.body.month),
+                Number(req.body.num))));
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(200).json(helper.error_message(e));
+    }
+}
+
+function eventGenerator (month, num) {
+    const arr = [];
+    for (let i = 1; i <= num; i++) {
+        const id = i;
+        const date = Math.floor(Math.random() * 24) + 1;
+        const hour = Math.floor(Math.random() * 24) + 1;
+        const nextHour = hour + 1;
+        arr.push({
+            'id': id,
+            'title': `Appointment ${id}`,
+            'start': `2020-0${month}-${date}T${hour}:00:00`,
+            'end': `2020-0${month}-${date}T${nextHour}:00:00`,
+            'color': '#bad066',
+            'backgroundColor': '#f3f0f0',
+            'allDay': false,
+            'textColor': '#000000',
+            'extendedProps': {
+                data: 'Data'
+            }
+        });
+    }
+    return arr;
+}
--- routes/data.js
+++ routes/data.js
@@ -4,4 +4,5 @@ const controller = require("../controller/dataCtrl");
 
 exports.data = (app) => {
     app.post('/data', controller.reportsData);
+    app.post('/event', controller.eventData);
 };
